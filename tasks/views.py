10e95bfdf4c98c5156d29b3eadb77dbfa452fb6a
Nefta11 <neftaliarturohernandez@gmail.com>
2024-08-28 15:13:22 -0600

    Add form

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 def helloWord(request):
-    return render(request,'signup.html')
+    return render(request,'signup.html',{
+        'form': UserCreationForm
+    })
