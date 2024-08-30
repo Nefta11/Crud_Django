c3e4fee7e7c8ab3a349372e661c5e66f00a5c7f5
Nefta11 <neftaliarturohernandez@gmail.com>
2024-08-29 19:29:08 -0600

    Updated

--- tasks/views.py
+++ tasks/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 def home(request):
     return render(request,'home.html')
 
 def signup(request):
+        
         if request.method == 'GET':
-            print('Enviando formulario')
-        else:
-            print(request.POST)
-            print('Obteniendo datos')
-        return render(request,'signup.html',{
-            'form':UserCreationForm
+            return render(request,'signup.html',{
+                'form':UserCreationForm
         })
+        else:
+            if request.POST['paswword1'] == request.POST['paswword1']:
+                user =  User.objects.create_user(username=request.POST('username'), password= request.POST['password1'])
+                user.save()
+                return HttpResponse('User created succesfully')
+                #Register User
+            return HttpResponse('Password do not match')
+    
